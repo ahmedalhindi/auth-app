5ddf04f8c0560f994a46171a9d1257de12b50f7b
ahmedalhindi <alhindi.ahmed@gmail.com>
2021-05-28 21:40:59 +0300

    express, mongoose

---
+++ api/src/app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/ping",(req,res)=>res.status(200).send('pong'))
+
+module.exports = app
--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,7 @@
-console.log('helloworld')
+const app = require("./app");
+const db = require("./db");
+
+db.connect()
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Express on port `+ port));
